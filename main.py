d7cbffc582cc03f6fb34ffcf37074e84f7cbc8ae
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2025-09-29 23:13:17 -0300

    ✨ feat(cli): add command-line argument parsing for book path
    - accept book path as a command-line argument
    - display usage message and exit if no argument provided

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_book_character_count,
     get_book_word_count,
@@ -12,13 +14,20 @@ def get_book_text(filepath: str) -> str:
 
 
 def main():
-    file_path = "books/frankenstein.txt"
+    args = sys.argv
+
+    if len(args) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = args[1]
     book_text = get_book_text(file_path)
     word_count = get_book_word_count(book_text)
     char_count = get_book_character_count(book_text)
     sorted_char_count = sort_character_dicts(char_count)
     report = get_report(file_path, word_count, sorted_char_count)
     print(report)
+
     # print(sorted_char_count)
 
 
